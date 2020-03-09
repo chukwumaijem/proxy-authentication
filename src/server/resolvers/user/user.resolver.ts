9a3a1779876dba03cbde16272501a06242a5e49f
Chukwuma Ezumezu <chukwuma.ezumezu@gmail.com>
2020-03-09 11:39:55 +0100

    remove db code from resolver

--- src/server/resolvers/user/user.resolver.ts
+++ src/server/resolvers/user/user.resolver.ts
@@ -1,9 +1,12 @@
 import { Resolver, Arg, Query, Mutation, Args } from 'type-graphql';
 import { User } from '../../entities';
 import { UserArgsType } from '../../types';
+import { UserService } from './user.service';
 
 @Resolver()
 export class UserResolver {
+  constructor(private userService: UserService) {}
+
   @Query()
   hello(@Arg('name') name: string): string {
     return `Hello, ${name}`;
@@ -11,34 +14,16 @@ export class UserResolver {
 
   @Mutation(() => User)
   addUser(@Args() userData: UserArgsType): Promise<User> {
-    try {
-      const user = User.create({ ...userData });
-
-      return user.save();
-    } catch (error) {
-      throw new Error(error);
-    }
+    return this.userService.addUser(userData);
   }
 
   @Query(() => User)
   async getUser(@Arg('id') userId: number): Promise<User | undefined> {
-    try {
-      const user = await User.findOne(userId);
-
-      return user;
-    } catch (error) {
-      throw new Error(error);
-    }
+    return this.userService.getUser(userId);
   }
 
   @Query(() => [User])
   async getUsers(): Promise<User[]> {
-    try {
-      const users = await User.find();
-
-      return users;
-    } catch (error) {
-      throw new Error(error);
-    }
+    return this.userService.getUsers();
   }
 }
